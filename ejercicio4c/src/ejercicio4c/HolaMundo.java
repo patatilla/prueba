fad5e99a2a7f254c9a85aca74cb0f5753ad3ae91
Victor <cosasvictor01@gmail.com>
2021-03-25 11:36:49 +0100

    Nueva versión
    He añadido funcionalidad.
    
    Signed-off-by: Victor <cosasvictor01@gmail.com>

--- ejercicio4c/src/ejercicio4c/HolaMundo.java
+++ ejercicio4c/src/ejercicio4c/HolaMundo.java
@@ -3,8 +3,10 @@ package ejercicio4c;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
-
+		System.out.println("Hola");
+		System.out.println("Mundo");
+		System.out.println("Qué tal?");
+		System.out.println("Qué tal?");
 	}
 
 }
